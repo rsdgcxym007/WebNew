1aa0399c59ff95611533ceda476512fa4bc38a8b
590107030026 <590107030026@dpu.ac.th>
2022-02-22 19:56:14 +0700

    datatableuser

---
+++ components/datatableuser.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-container>
+      <v-card>
+        <v-card-title>
+          ผู้ป่วยที่อยู่ในการแล
+          <v-spacer></v-spacer>
+          <v-text-field
+            v-model="search"
+            append-icon="mdi-magnify"
+            label="ค้นหา"
+          ></v-text-field>
+        </v-card-title>
+        <v-data-table
+          :headers="headers"
+          :items="details"
+          :items-per-page="5"
+          :search="search"
+          class="elevation-1"
+        >
+          <template v-slot:[`item.status_name`]="{ item }">
+            <v-chip :color="item.color">
+              {{ item.status_name }}
+            </v-chip>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['headers', 'details'],
+  data() {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    // request() {
+    //   this.$router.push({ path: '/manage/request' })
+    // },
+    // upcovid() {
+    //   this.$router.push({ path: '/manage/upcovid' })
+    // },
+    // update() {
+    //   this.$router.push({ path : '/manage/update'})
+    // },
+    // async logout() {
+    //   await this.$auth.logout()
+    //   this.$router.push('/')
+    // },
+  },
+}
+</script>
--- pages/volunteen/helpvolunteer.vue
+++ pages/volunteen/helpvolunteer.vue
@@ -1,152 +1,33 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="desserts"
-    sort-by="calories"
-    class="elevation-1"
-  >
-    <template v-slot:top>
-      <v-toolbar flat>
-        <v-toolbar-title>ชื่อผู้ป่วย</v-toolbar-title>
-        <v-divider class="mx-16" inset vertical></v-divider>
-        <v-spacer><v-card-text> สถานะ : ยังไม่หายป่วย </v-card-text></v-spacer>
-        <v-spacer><v-card-text> ระดับอาการ : แดง </v-card-text></v-spacer>
-        <v-dialog v-model="dialog" max-width="500px">
-          <v-card>
-            <v-card-title>
-              <span class="text-h5">{{ formTitle }}</span>
-            </v-card-title>
-
-            <v-card-text>
-              <v-container>
-                <v-row>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.symptom"
-                      label="อาการ"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.temperature"
-                      label="อุณหภูมิ"
-                    ></v-text-field>
-                  </v-col>
-                  <v-col cols="12" sm="6" md="4">
-                    <v-text-field
-                      v-model="editedItem.date"
-                      label="วันที่"
-                    ></v-text-field>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-      </v-toolbar>
-    </template>
-    <template v-slot:item.actions="{ item }">
-      <v-icon small class="mr-2" @click="editItem(item)"> mdi-pencil </v-icon>
-      <v-icon small @click="deleteItem(item)"> mdi-delete </v-icon>
-    </template>
-    <template v-slot:no-data>
-      <v-btn color="primary" @click="initialize"> Reset </v-btn>
-    </template>
-  </v-data-table>
+  <div>
+    <Datatableuser :headers="headers" :details="details" />
+  </div>
 </template>
+
 <script>
 export default {
-  data: () => ({
-    dialog: false,
-    dialogDelete: false,
-    headers: [
-      {
-        text: 'อาการ',
-        align: 'start',
-        sortable: false,
-        value: 'symptom',
-      },
-      { text: 'อุณหภูมิ', value: 'temperature' },
-      { text: 'วันที่', value: 'date' },
-    ],
-    desserts: [],
-    editedIndex: -1,
-    editedItem: {
-      symptom: '',
-      temperature: '',
-      date: '',
-    },
-    defaultItem: {
-      symptom: '',
-      temperature: '',
-      date: '',
-    },
-  }),
-
-  computed: {
-    formTitle() {
-      return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
-    },
+  middleware: 'auth',
+  async fetch() {
+    await this.fetchData()
   },
-
-  watch: {
-    dialog(val) {
-      val || this.close()
-    },
-    dialogDelete(val) {
-      val || this.closeDelete()
-    },
+  data() {
+    return { details: [], headers: [], search: '' }
   },
-
-  created() {
-    this.initialize()
-  },
-
   methods: {
-    initialize() {
-      this.desserts = []
+    async fetchData() {
+      const { result: tasks, headers } = await this.$axios.$post(
+        '/api/taskall',
+        { userId: this.$auth.user.id }
+      )
+      this.details = tasks
+      this.headers = headers
     },
-
-    editItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+    request() {
+      this.$router.push({ path: '/manage/request' })
     },
-
-    deleteItem(item) {
-      this.editedIndex = this.desserts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
-    },
-
-    deleteItemConfirm() {
-      this.desserts.splice(this.editedIndex, 1)
-      this.closeDelete()
-    },
-
-    close() {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    closeDelete() {
-      this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
-    },
-
-    save() {
-      if (this.editedIndex > -1) {
-        Object.assign(this.desserts[this.editedIndex], this.editedItem)
-      } else {
-        this.desserts.push(this.editedItem)
-      }
-      this.close()
+    async logout() {
+      await this.$auth.logout()
+      this.$router.push('/')
     },
   },
 }
