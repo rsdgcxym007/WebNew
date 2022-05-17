5f8b665913347f1019747ceef2ab40c089b8d5d3
GoyChawisa <59275070+GoyChawisa@users.noreply.github.com>
2022-05-17 22:08:36 +0700

    filter

--- components/datatableuser.vue
+++ components/datatableuser.vue
@@ -5,11 +5,24 @@
         <v-card-title>
           ผู้ป่วยที่อยู่ในการแล
           <v-spacer></v-spacer>
-          <v-text-field
-            v-model="search"
-            append-icon="mdi-magnify"
-            label="ค้นหา"
-          ></v-text-field>
+          <v-col cols="6">
+            <v-row>
+              <v-col cols="8">
+                <v-text-field
+                  v-model="search"
+                  append-icon="mdi-magnify"
+                  label="ค้นหา"
+                ></v-text-field
+              ></v-col>
+              <v-col cols="4"
+                ><v-select
+                  v-model="status"
+                  class="ml-4"
+                  :items="statusItems"
+                  label="สถานะ"
+                  @change="onChangeStatus"
+                ></v-select></v-col></v-row
+          ></v-col>
         </v-card-title>
         <v-data-table
           :headers="headers"
@@ -20,12 +33,12 @@
           @click:row="editItem"
         >
           <template v-slot:[`item.status_name`]="{ item }">
-            <v-chip :color="item.color">
+            <v-chip dark :color="item.color">
               {{ item.status_name }}
             </v-chip>
           </template>
           <template v-slot:[`item.level`]="{ item }">
-            <v-chip :color="item.level">
+            <v-chip :color="item.level" dark>
               <div v-if="item.level == 'green'">เขียว</div>
               <div v-if="item.level == 'yellow'">เหลือง</div>
               <div v-if="item.level == 'red'">แดง</div>
@@ -38,10 +51,30 @@
 </template>
 <script>
 export default {
-  props: ['headers', 'details'],
+  props: {
+    headers: {
+      type: Array,
+      default: [],
+    },
+    details: {
+      type: Array,
+      default: [],
+    },
+    onChangeStatus: {
+      type: Function,
+      default: () => {},
+    },
+  },
   data() {
     return {
+      status: 'ทั้งหมด',
       search: '',
+      statusItems: [
+        'ทั้งหมด',
+        'กำลังช่วยเหลือ',
+        'ช่วยเหลือเสร็จสิ้น',
+        'หายป่วยแล้ว',
+      ],
     }
   },
   methods: {
--- pages/volunteen/takecareuser.vue
+++ pages/volunteen/takecareuser.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <Datatableuser :headers="headers" :details="details" />
+    <Datatableuser
+      :headers="headers"
+      :details="details"
+      :onChangeStatus="onChangeStatus"
+    />
   </div>
 </template>
 
@@ -11,17 +15,25 @@ export default {
     await this.fetchData()
   },
   data() {
-    return { details: [], headers: [], search: '' }
+    return { details: [], headers: [], search: '', statusName: '' }
   },
   methods: {
     async fetchData() {
       const { result: tasks, headers } = await this.$axios.$post(
         '/api/volunteen/takecareuser',
-        { userId: this.$auth.user.id }
+        { userId: this.$auth.user.id, statusName: this.statusName }
       )
       this.details = tasks
       this.headers = headers
-      console.log('takecareuser',tasks)
+      console.log('takecareuser', tasks)
+    },
+    onChangeStatus(val) {
+      if (val == 'ทั้งหมด') {
+        this.statusName = ''
+      } else {
+        this.statusName = val
+      }
+      this.fetchData()
     },
   },
 }
